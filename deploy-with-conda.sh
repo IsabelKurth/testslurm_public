c65c0a2899008cf58721cd5f518003862c2072dc
IsabelKurth <83709330+IsabelKurth@users.noreply.github.com>
2022-06-20 15:24:08 +0200

    Create deploy-with-conda.sh

---
+++ deploy-with-conda.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#SBATCH --ntasks=1                # Number of tasks (see below)
+#SBATCH --cpus-per-task=1         # Number of CPU cores per task
+#SBATCH --nodes=1                 # Ensure that all cores are on one machine
+#SBATCH --time=0-00:05            # Runtime in D-HH:MM
+#SBATCH --partition=gpu-2080ti-dev # Partition to submit to
+#SBATCH --gres=gpu:1              # optionally type and number of gpus
+#SBATCH --mem=50G                 # Memory pool for all cores (see also --mem-per-cpu)
+#SBATCH --output=logs/job_%j.out  # File to which STDOUT will be written
+#SBATCH --error=logs/job_%j.err   # File to which STDERR will be written
+#SBATCH --mail-type=FAIL           # Type of email notification- BEGIN,END,FAIL,ALL
+#SBATCH --isabel.kurth@t-online.de  # Email to which notifications will be sent
+
+# print info about current job
+echo "---------- JOB INFOS ------------"
+scontrol show job $SLURM_JOB_ID 
+echo -e "---------------------------------\n"
+
+# Due to a potential bug, we need to manually load our bash configurations first
+source $HOME/.bashrc
+
+# Next activate the conda environment 
+conda activate myenv
+
+# Run our code
+echo "-------- PYTHON OUTPUT ----------"
+python3 hello.py --timer_repetitions 10000 --use-gpu
+echo "---------------------------------"
+
+# Deactivate environment again
+conda deactivate
